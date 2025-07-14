0d9cbb2268c0a8ed32ef2d9356bc7dd70baeb7c2
joon shakya <iwilllearnjs@gmail.com>
2025-07-14 11:34:31 +0545

    hello

---
+++ assinament.php
@@ -0,0 +1,6 @@
+<?php
+$a = 3;
+$b = 6;
+$a += $b;
+echo "result is ".$a;
+?>
---
+++ condition.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+<?php 
+$age = 20;
+if ($age >= 18){
+	echo "you are adult"."<br>";
+}
+?>
+
+
+
+<?php 
+$age = 15;
+if ($age>= 18){
+	echo " you are adult";
+}else{
+	echo "you are minor";
+}
+?>
+</body>
+</html>
